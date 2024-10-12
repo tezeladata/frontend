d9521c45ff6291dad7c22ca329e24e9a1cb0f6d9
tezeladata <David.tezela@gmail.com>
2024-10-12 16:49:22 +0400

    responsive fix

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,7 +2,7 @@ import FooterMain from "../Components/FooterMain";
 
 const Footer = () => {
     return (
-        <section className="bg-gray-300 pt-20">
+        <section className="bg-gray-300 pt-20 h-screen">
             <FooterMain />
         </section>
     )
