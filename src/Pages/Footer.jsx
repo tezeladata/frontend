c9f0b84bb92c380af1fa54f84c320776f65c22f1
tezeladata <David.tezela@gmail.com>
2024-10-12 15:33:30 +0400

    responsive fix

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,7 +2,7 @@ import FooterMain from "../Components/FooterMain";
 
 const Footer = () => {
     return (
-        <section className="bg-gray-300 pt-20 max-[1000px]:pt-56">
+        <section className="bg-gray-300 pt-20 max-[1000px]:pt-96">
             <FooterMain />
         </section>
     )
--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -3,7 +3,7 @@ import About from "../Components/About.jsx";
 
 const Info = () => {
     return (
-        <section className="min-h-min bg-gray-300">
+        <section className="min-h-min bg-gray-300 max-[800px]:mb-40">
             <Cards />
             <About />
         </section>
